417565484594a521b270ba342c6ce96de1ce9190
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2024-02-23 14:50:42 +0530

    Done with basics of string
    learnt about string and the functions which we can perform on the string

---
+++ tut_12.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class tut_12 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // String full_name= sc.nextLine();
+        // System.out.println(full_name);
+
+
+        // Concatenation
+        // String firstName=sc.next();
+        // String lastName=sc.next();
+        // String fullName=firstName +" "+lastName;
+        // System.out.println(fullName);
+
+        // Length of string
+
+        // System.out.println(fullName.length());
+
+        // CharAt()
+
+        // for(int j=0;j<fullName.length();j++);
+        // {
+        //     System.out.println(fullName.charAt(j));
+        // }
+
+        // Compare 
+         
+        // String s1="gupta";
+        // String s2="Gupta";
+        // if(s1.compareToIgnoreCase(s2)==0)
+        // {
+        //     System.out.println("Both are equal");
+        // }
+        // else if(s1.compareToIgnoreCase(s2)>0)
+        // {
+        //     System.out.println("s1 is greater");
+        // }
+        // else{
+        //     System.out.println("s2 is greater");
+        // }
+
+
+
+        // Substring of any string
+
+        String str="My name is";
+        System.out.println(str.substring(3)); 
+
+        
+
+
+    }
+    
+}
